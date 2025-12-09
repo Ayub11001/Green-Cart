c821f83350255586c7cca4a2e159ea6a04fef70a
Ayub <sameerbaig690@gmail.com>
2025-12-10 02:32:23 +0530

    Update AppContext to set isSeller to false; implement Orders and ProductList components with order fetching and product listing functionality

--- client/src/pages/seller/AddProducts.jsx
+++ client/src/pages/seller/AddProducts.jsx
@@ -25,8 +25,7 @@ const AddProducts = () => {
                     <div className="flex flex-wrap items-center gap-3 mt-2">
                         {Array(4).fill('').map((_, index) => (
                             <label key={index} htmlFor={`image${index}`}>
-                                <input accept="image/*" type="file" id={`image${index}`} hidden />
-                                <img 
+                                <input 
                                 onChange={
                                     (e) => {
                                         const updatedFiles = [...files];
@@ -34,6 +33,12 @@ const AddProducts = () => {
                                         setFiles(updatedFiles);
                                     }
                                 }
+                                accept="image/*" 
+                                type="file" 
+                                id={`image${index}`} 
+                                hidden />
+                                <img 
+                                
                                 className="max-w-24 cursor-pointer" 
                                 src={files[index] ? URL.createObjectURL(files[index]) : assets.upload_area} alt="uploadArea" 
                                 width={100} height={100} 
