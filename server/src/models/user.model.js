cdf26bbed57e110f6971fe537a5af69d8bb21744
Ayub <sameerbaig690@gmail.com>
2026-01-10 18:01:37 +0530

    Fix password hashing middleware to use correct context and ensure proper next function call

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -28,12 +28,13 @@ const userSchema = new mongoose.Schema({
     minimize: false  
 });
 
-userSchema.pre('save', async (next) => {
-    if(!this.isModified('password')) {
-        return next()
+userSchema.pre('save', async function(){
+    if(!(this.isModified('password'))) {
+        return ;
     }
 
-    this.password = await bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10);
+    
 })
 
 userSchema.methods.isPasswordCorrect = async (password) => {
